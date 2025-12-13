8747ef25cfc46d9aa55eae073d6361aa24d0606b
John Evans <john.a.evans@gmail.com>
2025-12-13 07:46:33 -0800

    Initial Commit

---
+++ Spryte/SpryteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpryteApp.swift
+//  Spryte
+//
+//  Created by John Evans on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SpryteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
